45565696f55eb674519ca169e8784bc70e487628
peidacai <cyrus_15@rocketmail.com>
2016-12-12 00:01:19 -0500

    Week13Day0

---
+++ projects/projects-capstone/part-05/retail_rental_app.py
@@ -0,0 +1,82 @@
+import flask
+app = flask.Flask(__name__)
+
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
+import pickle
+
+# with open('/Users/peidacai/GA-DSI/projects/projects-capstone/part-05/assets/taxi_ref_df.pkl', 'r') as f:
+#     taxi_ref_df = pickle.load(f)
+
+# with open('/Users/peidacai/GA-DSI/projects/projects-capstone/part-05/assets/full_yelp_ref_df.pkl', 'r') as f:
+#     yelp_ref_df = pickle.load(f)
+
+# with open('/Users/peidacai/GA-DSI/projects/projects-capstone/part-05/assets/model.pkl', 'r') as f:
+#     model = pickle.load(f)
+
+# with open('/Users/peidacai/GA-DSI/projects/projects-capstone/part-05/assets/x_scaler.pkl', 'r') as f:
+#     ss_x = pickle.load(f)
+
+# with open('/Users/peidacai/GA-DSI/projects/projects-capstone/part-05/assets/y_scaler.pkl', 'r') as f:
+#     ss_y = pickle.load(f)
+
+# with open('/Users/peidacai/GA-DSI/projects/projects-capstone/part-05/assets/x_zip.pkl', 'r') as f:
+#     x_zip = pickle.load(f)
+
+
+
+# def set_zip(zipcode, zip_df):
+#     if zipcode == '10001':
+#         return zip_df
+#     else:
+#         try:
+#             for i in zip_df.columns:
+#                 new_i = i.replace('Zip__', '')
+#                 if new_i == zipcode:
+#                     zip_df.loc[0, i] = 1
+#                     return zip_df
+#                 else:
+#                     return zip_df
+#         except:
+#             return zip_df
+
+@app.route("/page")
+def page():
+    with open('page.html', 'r') as viz_file:
+        return viz_file.read()
+
+@app.route('/predict', methods = ['POST', 'GET'])
+def predict():
+    if flask.request.method == 'POST':
+
+        inputs = flask.request.form
+
+      
+        #zipcode = inputs['zipcode']
+
+        #area = inputs['area']
+
+        #c = taxi_ref_df.loc[zipcode, ['dropoff_hr_0600_1200', 'dropoff_hr_1200_1800', 'dropoff_hr_1800_2359', 'dropoff_hr_2359_0600']]
+        # sf = pd.Series([area], index = ['SF_avail'])
+        # d = yelp_ref_df.loc[zipcode, :]
+
+        # inp = pd.DataFrame(pd.concat([sf, d, c], axis = 0))
+        # inp = inp.T
+
+        # inpu = pd.DataFrame(ss_x.transform(inp), columns = inp.columns)
+        # inp_x = pd.concat([inpu, set_zip(str(zipcode), x_zip)], axis = 1)
+
+        # y_pred = round(ss_y.inverse_transform(model.predict(inp_x))[0],2)
+
+
+        return 'hi'
+
+
+
+
+if __name__ == '__main__':
+    HOST = '127.0.0.1'
+    PORT = '5000'
+    app.run(HOST, PORT)
